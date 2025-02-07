4d229fadb7979f9368c7587785abfe6fe65086f6
oimos <shinyatkhs@gmail.com>
2025-02-07 09:34:58 +0900

    feat: add content

--- public/script.js
+++ public/script.js
@@ -5,28 +5,28 @@ sdk.sitemap()
 
 sdk.sendEvent('click', { target: 'button' })
 
-document.getElementById("fetchMessage").addEventListener("click", async () => {
-  try {
-    const response = await fetch("/api/user/2");
-    const data = await response.json();
-    document.getElementById("message").innerText = JSON.stringify(data);
-  } catch (error) {
-    console.error("Error fetching message:", error);
-  }
-});
+// document.getElementById("fetchMessage").addEventListener("click", async () => {
+//   try {
+//     const response = await fetch("/api/user/2");
+//     const data = await response.json();
+//     document.getElementById("message").innerText = JSON.stringify(data);
+//   } catch (error) {
+//     console.error("Error fetching message:", error);
+//   }
+// });
 
-(async () => {
-  const weatherResponse = await fetch('https://weather.tsukumijima.net/api/forecast/city/130010')
-  const weatherData = await weatherResponse.json()
-  console.log(weatherData)
+// (async () => {
+//   const weatherResponse = await fetch('https://weather.tsukumijima.net/api/forecast/city/130010')
+//   const weatherData = await weatherResponse.json()
+//   console.log(weatherData)
 
-  const {
-    title,
-    forecasts
-  } = weatherData
-  console.log(title)
-  console.log(forecasts[0].date, forecasts[0].telop)
-})()
+//   const {
+//     title,
+//     forecasts
+//   } = weatherData
+//   console.log(title)
+//   console.log(forecasts[0].date, forecasts[0].telop)
+// })()
 
 
 
